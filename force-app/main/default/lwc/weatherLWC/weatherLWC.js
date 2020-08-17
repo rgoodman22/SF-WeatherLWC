9ed462fb81aae803940068ecfafeaa1ab39d5f59
Robert Goodman <robert.goodman@robertgood-ltm.internal.salesforce.com>
2020-08-17 15:45:50 -0400

    Working on alignment of subcard

--- force-app/main/default/lwc/weatherLWC/weatherLWC.js
+++ force-app/main/default/lwc/weatherLWC/weatherLWC.js
@@ -1,6 +1,7 @@
 import { LightningElement, api, wire, track } from 'lwc';
 import  call  from "@salesforce/apex/weatherLWC.call";
 import pop  from "@salesforce/apex/weatherLWC.pop";
+import weatherIcon from "@salesforce/resourceUrl/weatherIcon";
 
 
 export default class weatherLWC extends LightningElement {
@@ -9,24 +10,62 @@ export default class weatherLWC extends LightningElement {
     @api minutelyData;
     @api hourlyData;
     @api dailyData;
-    
-    @wire(pop) locations;
-    selectedLoc;
 
-    handleSelect(id) {
-        if (selectedLoc != id) {
-            let element;
-            element = document.getElementById(selectedLoc);
-            element.className = element.className.replace("slds-is-selected", "");
-            element.getAttribute("aria-selected", false);
-            element.getAttribute("aria-checked", false);
-            
-            selectedLoc = id;
-            element = document.getElementById(id);
-            element.className = element.className + " slds-is-selected";
-            element.setAttribute("aria-selected", true);
-            element.setAttribute("aria-checked", true);
+    svgURL = `${weatherIcon}#snowy-3`;
+    
+
+    
+    @track data;
+    @track error;
+    @track value = 'Search a location...';
+    @track name;
+    isLocationSelected = false;
+    
+    @wire(pop) 
+    popLocations ({error, data}) {
+        if (data) {
+            this.data = data;
+            this.error = undefined;
+        } else {
+            this.error = error;
+            this.data = undefined;
         }
     }
+
+    get options() {
+        if (this.data) {
+            let myOptions = [];
+            let length = this.data.length;
+            for (let i = 0; i < length; i++){
+                let x = this.data[i];
+                myOptions.push({label: x.Name, value: x.Id});
+
+            }
+            return myOptions;
+        } else{
+            return this.error;
+        }
+        
+    }
+
+
+    handleselect(event) {
+        this.value = event.detail.value;
+        this.isLocationSelected = true;
+    }
+
+    get getName() {
+        if (this.data) {
+            let length = this.data.length;
+            for (let i = 0; i<length; i++) {
+                let x = this.data[i];
+                if (this.value === x.Id) {
+                    return x.Name;
+                }
+            }
+        }
+    }
+
+
 
 }
